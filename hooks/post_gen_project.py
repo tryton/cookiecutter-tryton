23aed5ca4b3ec57621fed9d93689f910d9ed0c7b
Cédric Krier <ced@b2ck.com>
2022-03-14 17:26:58 +0100

    Remove testing raise of OSError

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,6 @@ import os
 import shutil
 
 try:
-    raise OSError()
     os.symlink('doc/index.rst', 'README.rst')
 except (AttributeError, OSError):
     shutil.copyfile('doc/index.rst', 'README.rst')
