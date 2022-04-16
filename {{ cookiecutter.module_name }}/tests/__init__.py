71183f833d08f4d4507bdc8ca5f173d19fe30740
Cédric Krier <ced@b2ck.com>
2022-04-16 18:30:48 +0200

    Replace test setuptools command by unittest discover
    issue9215
    review409111002

--- {{ cookiecutter.module_name }}/tests/__init__.py
+++ {{ cookiecutter.module_name }}/tests/__init__.py
@@ -1,11 +1,4 @@
 {% if not cookiecutter.prefix -%}
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
 {% endif -%}
-try:
-    from trytond.modules.{{ cookiecutter.module_name }}.tests.test_{{cookiecutter.module_name }} import suite  # noqa: E501, isort: skip
-except ImportError:
-    from .test_{{ cookiecutter.module_name }} import suite
-
-__all__ = ['suite']
